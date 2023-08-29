891e79c25ee8a7ad35bf4ead41591371f10ccc0a
Oren Drori <oren.dro@gmail.com>
2023-08-29 19:37:35 +0300

    main

--- src/components/CardsContainer/CardsContainer.tsx
+++ src/components/CardsContainer/CardsContainer.tsx
@@ -5,9 +5,10 @@ import './CardsContainer.css'
 
 
 function CardsContainer() {
-    let [productsModel, setProductsModel] = useState<IProduct[]>([{ id: 0, name: 'Computer Screen', description: '26" 4K LED display', company: 'LG', price: 1200, amount: 7},
-    { id: 1, name: 'Whiskey', description: 'Blended scotch whiskey', company: 'Jack Daniels', price: 100, amount: 50},
-    { id: 2, name: 'The third product', description: 'Some random thing. I could not think of anything original', company: 'The one and only', price: 200, amount: 13}]);
+    let [productsModel, setProductsModel] = useState<IProduct[]>([
+        { id: 0, name: 'Computer Screen', description: '26" 4K LED display', company: 'LG', price: 1200, amount: 7},
+        { id: 1, name: 'Whiskey', description: 'Blended scotch whiskey', company: 'Jack Daniels', price: 100, amount: 50},
+        { id: 2, name: 'The third product', description: 'Some random thing. I could not think of anything original', company: 'The one and only', price: 200, amount: 13}, { id: 3, name: 'The fourth product', description: 'Some random thing. I could not think of anything original', company: 'The one and only', price: 200, amount: 13}]);
 
     let [productsViewModel, setProductsViewModel] = useState<IProduct[]>(productsModel);
     let [userType, setUserType] = useState('user');
